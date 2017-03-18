afc956fb38b63c10846cc7467de0b0916b8acdaf
Jacqueline <Jacqueline@192.168.178.40>
2017-03-18 14:28:01 +0100

    neu

--- aufgabe0/js/aufgabe0.js
+++ aufgabe0/js/aufgabe0.js
@@ -1,1 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var eingabe = prompt("Bitte Namen eingeben");
+    document.getElementById("hallo").innerHTML = "Herzlich Willkommen";
+});
 //# sourceMappingURL=aufgabe0.js.map
---
+++ aufgabe0/js/aufgabe0.ts
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+var eingabe: string  = prompt ("Bitte Namen eingeben");
+ 
+document.getElementById("hallo").innerHTML = "Herzlich Willkommen";  
+
+});
