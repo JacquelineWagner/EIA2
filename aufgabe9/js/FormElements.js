7264a819558d45e2bb1e49cb8abfbab83d2e46c1
Jacqueline <Jacqueline@192.168.178.40>
2017-06-03 13:46:32 +0200

    bearbeitet

--- aufgabe9/js/FormElements.js
+++ aufgabe9/js/FormElements.js
@@ -1,36 +1,84 @@
 var Form;
 (function (Form) {
     window.addEventListener("load", init);
+    let eissorten = ["Vanille", "Schokolade", "Himbeere", "Erdbeere", "Nuss",
+        "Zitrone"];
+    let toppings = ["Sahne", "Schokosauce", "Karamelsauce", "Streusel",
+        "Fruechte-Mix"];
+    let bestellung = [];
+    //    let step: HTMLInputElement = document.createElement("input");            
+    //    let div: HTMLDivElement = document.createElement("div");    
+    //      
+    let inputs = [];
+    let fieldsetTopping;
+    let fieldsetEissorte;
     function init(_event) {
-        console.log("Init");
-        let fieldsets = document.getElementsByTagName("fieldset");
-        for (let i = 0; i < fieldsets.length; i++) {
-            let fieldset = fieldsets[i];
-            fieldset.addEventListener("change", handleChange);
+        fieldsetEissorte = document.getElementById("Eissorten");
+        fieldsetTopping = document.getElementById("toppings");
+        for (let i = 0; i < eissorten.length; i++) {
+            let input = document.createElement("input");
+            let label = document.createElement("label");
+            input.setAttribute("type", "number");
+            input.setAttribute("value", "0");
+            label.innerText = eissorten[i];
+            label.appendChild(input);
+            inputs.push(input);
+            fieldsetEissorte.appendChild(label);
+            input.className = "checkbox";
+            console.log(eissorten[i]);
         }
+        for (let i = 0; i < toppings.length; i++) {
+            let input = document.createElement("input");
+            let label = document.createElement("label");
+            input.setAttribute("type", "checkbox");
+            label.innerText = toppings[i];
+            label.appendChild(input);
+            inputs.push(input);
+            fieldsetTopping.appendChild(label);
+            input.className = "checkbox";
+            console.log(toppings[i]);
+        }
+        //        step.addEventListener("click", clickEvent);
     }
-    function handleChange(_event) {
-        //console.log(_event);
-        //*/
-        let target = _event.target;
-        console.log("Changed " + target.name + " to " + target.value);
-        //*/
-        //*/ note: this == _event.currentTarget in an event-handler
-        if (this.id == "checkbox")
-            console.log("Changed " + target.name + " to " + target.checked);
-        //*/
-        //*/
-        if (target.name == "Slider") {
-            let progress = document.getElementsByTagName("progress")[0];
-            progress.value = parseFloat(target.value);
-        }
-        //*/
-        //*/
-        if (target.name == "Stepper") {
-            let progress = document.getElementsByTagName("meter")[0];
-            progress.value = parseFloat(target.value);
-        }
-        //*/
+    function clickEvent(_event) {
+        console.log("hallo");
     }
 })(Form || (Form = {}));
+//namespace Form {
+//    window.addEventListener("load", init);
+//
+//    function init(_event: Event): void {
+//        console.log("Init");
+//        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+//
+//        for (let i: number = 0; i < fieldsets.length; i++) {
+//            let fieldset: HTMLFieldSetElement = fieldsets[i];
+//            fieldset.addEventListener("change", handleChange);
+//        }
+//    }
+//
+//    function handleChange(_event: Event): void {
+//        //console.log(_event);
+//        //*/
+//        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+//        console.log("Changed " + target.name + " to " + target.value);
+//        //*/
+//        //*/ note: this == _event.currentTarget in an event-handler
+//        if (this.id == "checkbox")
+//            console.log("Changed " + target.name + " to " + target.checked);
+//        //*/
+//        //*/
+//        if (target.name == "Slider") {
+//            let progress: HTMLProgressElement = <HTMLProgressElement>document.getElementsByTagName("progress")[0];
+//            progress.value = parseFloat(target.value);
+//        }
+//        //*/
+//        //*/
+//        if (target.name == "Stepper") {
+//            let progress: HTMLProgressElement = <HTMLProgressElement>document.getElementsByTagName("meter")[0];
+//            progress.value = parseFloat(target.value);
+//        }
+//        //*/
+//    }
+//} 
 //# sourceMappingURL=FormElements.js.map
--- aufgabe9/js/FormElements.ts
+++ aufgabe9/js/FormElements.ts
@@ -1,37 +1,122 @@
 namespace Form {
+
     window.addEventListener("load", init);
 
+    let eissorten: string[] = ["Vanille", "Schokolade", "Himbeere", "Erdbeere", "Nuss",
+        "Zitrone"];
+
+    let toppings: string[] = ["Sahne", "Schokosauce", "Karamelsauce", "Streusel",
+        "Fruechte-Mix"];
+
+    let bestellung: string[] = [];
+
+
+    //    let step: HTMLInputElement = document.createElement("input");            
+    //    let div: HTMLDivElement = document.createElement("div");    
+    //      
+    let inputs: HTMLInputElement[] = [];
+    let fieldsetTopping: HTMLElement;
+    let fieldsetEissorte: HTMLElement;
+
     function init(_event: Event): void {
-        console.log("Init");
-        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
 
-        for (let i: number = 0; i < fieldsets.length; i++) {
-            let fieldset: HTMLFieldSetElement = fieldsets[i];
-            fieldset.addEventListener("change", handleChange);
+        fieldsetEissorte = document.getElementById("Eissorten");
+        fieldsetTopping = document.getElementById("toppings");
+
+
+        for (let i: number = 0; i < eissorten.length; i++) {
+            let input: HTMLInputElement = document.createElement("input");
+            let label: HTMLLabelElement = document.createElement("label");
+
+
+            input.setAttribute("type", "number");
+            input.setAttribute("value", "0");
+            label.innerText = eissorten[i];
+            label.appendChild(input);
+            inputs.push(input);
+            fieldsetEissorte.appendChild(label);
+
+            input.className = "checkbox";
+
+            console.log(eissorten[i]);
+
         }
+
+
+        for (let i: number = 0; i < toppings.length; i++) {
+
+            let input: HTMLInputElement = document.createElement("input");
+            let label: HTMLLabelElement = document.createElement("label");
+
+
+            input.setAttribute("type", "checkbox");
+            label.innerText = toppings[i];
+            label.appendChild(input);
+            inputs.push(input);
+            fieldsetTopping.appendChild(label);
+
+            input.className = "checkbox";
+
+            console.log(toppings[i]);
+
+        }
+
+        //        step.addEventListener("click", clickEvent);
     }
 
-    function handleChange(_event: Event): void {
-        //console.log(_event);
-        //*/
-        let target: HTMLInputElement = <HTMLInputElement>_event.target;
-        console.log("Changed " + target.name + " to " + target.value);
-        //*/
-        //*/ note: this == _event.currentTarget in an event-handler
-        if (this.id == "checkbox")
-            console.log("Changed " + target.name + " to " + target.checked);
-        //*/
-        //*/
-        if (target.name == "Slider") {
-            let progress: HTMLProgressElement = <HTMLProgressElement>document.getElementsByTagName("progress")[0];
-            progress.value = parseFloat(target.value);
-        }
-        //*/
-        //*/
-        if (target.name == "Stepper") {
-            let progress: HTMLProgressElement = <HTMLProgressElement>document.getElementsByTagName("meter")[0];
-            progress.value = parseFloat(target.value);
-        }
-        //*/
+
+    function clickEvent(_event: MouseEvent): void {
+        console.log("hallo");
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//namespace Form {
+//    window.addEventListener("load", init);
+//
+//    function init(_event: Event): void {
+//        console.log("Init");
+//        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+//
+//        for (let i: number = 0; i < fieldsets.length; i++) {
+//            let fieldset: HTMLFieldSetElement = fieldsets[i];
+//            fieldset.addEventListener("change", handleChange);
+//        }
+//    }
+//
+//    function handleChange(_event: Event): void {
+//        //console.log(_event);
+//        //*/
+//        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+//        console.log("Changed " + target.name + " to " + target.value);
+//        //*/
+//        //*/ note: this == _event.currentTarget in an event-handler
+//        if (this.id == "checkbox")
+//            console.log("Changed " + target.name + " to " + target.checked);
+//        //*/
+//        //*/
+//        if (target.name == "Slider") {
+//            let progress: HTMLProgressElement = <HTMLProgressElement>document.getElementsByTagName("progress")[0];
+//            progress.value = parseFloat(target.value);
+//        }
+//        //*/
+//        //*/
+//        if (target.name == "Stepper") {
+//            let progress: HTMLProgressElement = <HTMLProgressElement>document.getElementsByTagName("meter")[0];
+//            progress.value = parseFloat(target.value);
+//        }
+//        //*/
+//    }
+//}
